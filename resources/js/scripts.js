a40037bd3ca5aabb50f8518f4b653e3c9bf3bbe5
Matthew Maddox <mathmadd23@hotmail.com>
2020-07-19 01:49:02 -0500

    CSS & Javascipt effects added

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+	/* Sticky Navigation */
 	$('.js--section-aboutme').waypoint(function(direction) {
 		if (direction == "down") {
 			$('nav').addClass('sticky')
@@ -9,19 +11,49 @@ $(document).ready(function() {
 		}
 		}, {
 		
-		offset: '60px;'
-});
+		offset: '20px;'
+    });
 
 
-// var waypoints = $('#handler-first').waypoint(function(direction) {
-//   notify(this.element.id + ' hit 25% from top of window') 
-// }, {
-//   offset: '25%'
+
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+
+// $('.js-nav-icon').click(function() {
+//   var nav = $('.js-main-nav');
+
+//   nav.slideToggle(200);
+
+
 // })
 
-
-
-
-
+/* Mobile navigation */
+$('.js-nav-icon').click(function() {
+  var nav = $('.js-main-nav');
+  // var icon = $('.js--nav-icon ion-icon');
+        
+  nav.slideToggle(200);
+    
+  if ($('#nav-mobile').attr('name','reorder-three')) {
+    $('#nav-mobile').attr('name','close');
+  } 
+  if ($('#nav-mobile').attr('name','close')) {
+    $('#nav-mobile').attr('name', 'reorder-three');
+  }        
+  });
 
 });
